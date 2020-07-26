72780367f185312b9e4c9ca41ce5a2a382ae3474
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-26 20:08:32 +0100

    function draw complete

--- js/main.js
+++ js/main.js
@@ -5,21 +5,41 @@ canvas.height = window.innerHeight;
 context.strokeStyle = "#bada55";
 context.lineJoin = "round";
 context.lineCap = "round";
+// context.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
-    if(!isDrawing) return;
-    console.log(e)  
+    if (!isDrawing) return;
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     context.beginPath();
     context.moveTo(lastX, lastY);
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (context.lineWidth >= 100 || context.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        context.lineWidth++;
+    } else {
+        context.lineWidth--;
+    }
 }
-canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => isDrawing = false);
